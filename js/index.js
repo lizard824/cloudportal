28f9c87c23b6b02c9a182f4996fc1166fd7d8652
duanxc1 <duanxc1@lenovo.com>
2016-12-23 10:50:38 +0800

    save local change

--- js/index.js
+++ js/index.js
@@ -32,7 +32,6 @@ $(document).ready(function () {
             } else {
                 toptip = "Error!";
             }
-            v_login.$set("valid", false);
             v_login.$set("error", toptip);
         }
     }
@@ -47,9 +46,7 @@ function iniLogin() {
             password: '',
             passValid: true,
             userValid: true,
-            valid: true,
-            error: '',
-            service: _CTX_ + "/sso/index"
+            error: ''
         },
         methods: {
             login: function () {
@@ -74,12 +71,14 @@ function iniLogin() {
         }
     });
     v_login.$watch("username", function (val) {
+        v_login.$set("error", "");
         if (val === "")
             v_login.$set("userValid", false);
         else
             v_login.$set("userValid", true);
     });
     v_login.$watch("password", function (val) {
+        v_login.$set("error", "");
         if (val === "")
             v_login.$set("passValid", false);
         else
@@ -108,7 +107,7 @@ function loadPage() {
         dataType: "json",
         success: function (data) {
             if (data.success == false) {
-                return data;
+                return;
             } else {
                 v_head.$set("isLogged", true);
                 v_head.$set("login", "Hello, " + data.realname);
@@ -124,11 +123,12 @@ function iniSign() {
             email: '',
             username: '',
             password: '',
-            name: '',
+            realname: '',
             passValid: true,
             userValid: true,
             emailValid: true,
-            nameValid: true
+            nameValid: true,
+            error: ''
         },
         methods: {
             sign: function () {
@@ -153,24 +153,28 @@ function iniSign() {
         }
     });
     v_sign.$watch("username", function (val) {
+        v_sign.$set("error", "");
         if (val === "")
             v_sign.$set("userValid", false);
         else
             v_sign.$set("userValid", true);
     });
     v_sign.$watch("password", function (val) {
+        v_sign.$set("error", "");
         if (val === "")
             v_sign.$set("passValid", false);
         else
             v_sign.$set("passValid", true);
     });
     v_sign.$watch("email", function (val) {
+        v_sign.$set("error", "");
         if (val === "" || !val.match(email))
             v_sign.$set("emailValid", false);
         else
             v_sign.$set("emailValid", true);
     });
-    v_sign.$watch("name", function (val) {
+    v_sign.$watch("realname", function (val) {
+        v_sign.$set("error", "");
         if (val === "")
             v_sign.$set("nameValid", false);
         else
@@ -180,15 +184,20 @@ function iniSign() {
         e.preventDefault(); // prevent native submit
         var username = v_sign.username;
         var email = v_sign.email;
-        var name = v_sign.name;
+        var name = v_sign.realname;
         var password = v_sign.password;
         if (username == '' || email == '' || name == '' || password == '') {
             return;
         } else {
             $(this).ajaxSubmit({
                 success: function (data) {
-                    if (data.success == true) {
+                    if (data.result == true) {
+                        myClo(signbj,sig);
                     } else {
+                        if (data.msg === "")
+                            v_sign.$set("error", "Sign in failed!");
+                        else
+                            v_sign.$set("error", data.msg);
                     }
                 }
             });
--- js/my.js
+++ js/my.js
@@ -1,213 +1,217 @@
-
 // rem布局----------------------------------------
-        document.documentElement.style.fontSize = innerWidth / 16+'px';
-	     onresize = function(){
-		document.documentElement.style.fontSize = innerWidth / 16+'px';
-	          };
+document.documentElement.style.fontSize = innerWidth / 16 + 'px';
+onresize = function () {
+    document.documentElement.style.fontSize = innerWidth / 16 + 'px';
+};
 
 
 // 登录注册切换中英文---------------------------------
 // 登录 窗口打开
-    var login  = document.getElementById('login');
-    var log    = document.getElementById('log');
-    var wlog   = document.getElementById('wlog');
-    var sign   = document.getElementById('sign');
-    var signbj = document.getElementById('signbj');
-    var sig    = document.getElementById('sig');
+var login = document.getElementById('login');
+var log = document.getElementById('log');
+var wlog = document.getElementById('wlog');
+var sign = document.getElementById('sign');
+var signbj = document.getElementById('signbj');
+var sig = document.getElementById('sig');
 
-    var clLig = function bl(a,b,c,d){
-             a.style.display = "block";
-             b.style.display = "block";          
-             c.style.display = "none";
-             d.style.display = "none"; 
-        };
-    login.onclick = function(){
-          clLig(log,wlog,signbj,sig);
-        };
-    sign.onclick = function(){
-          clLig(signbj,sig,log,wlog);
-    };
-
-
+var clLig = function bl(a, b, c, d) {
+    a.style.display = "block";
+    b.style.display = "block";
+    c.style.display = "none";
+    d.style.display = "none";
+};
+login.onclick = function () {
+    clLig(log, wlog, signbj, sig);
+};
+/*sign.onclick = function () {
+    $('#signForm').trigger("reset");
+    v_sign.$set("email", "");
+    v_sign.$set("username", "");
+    v_sign.$set("password", "");
+    v_sign.$set("realname", "");
+    v_sign.$set("error", "");
+    v_sign.$set("emailValid", true);
+    v_sign.$set("userValid", true);
+    v_sign.$set("passValid", true);
+    v_sign.$set("nameValid", true);
+    clLig(signbj, sig, log, wlog);
+};*/
 
 
 // 关闭-------------------------------------------------
-    var clos = document.getElementById('close');
-    var closig = document.getElementById('closesig');
+var clos = document.getElementById('close');
+var closig = document.getElementById('closesig');
 
 // 关闭窗口   
-    var myClo = function cl(a,b){
-         a.style.display = "none";
-         b.style.display = "none"; 
-    };
-
-clos.onclick = function(){
-     myClo(log,wlog);
-};
-closig.onclick = function(){
-     myClo(signbj,sig);
+var myClo = function cl(a, b) {
+    a.style.display = "none";
+    b.style.display = "none";
 };
 
+clos.onclick = function () {
+    myClo(log, wlog);
+};
+closig.onclick = function () {
+    myClo(signbj, sig);
+};
 
 
 // 导航背景------------------------------------------
-  var aUl = document.getElementById('xhead');
-  var twoDiv = document.getElementById('second');
+var aUl = document.getElementById('xhead');
+var twoDiv = document.getElementById('second');
 
 
-	onscroll=function(ev){
-		var ev = ev || event;
-		var top = document.documentElement.scrollTop || document.body.scrollTop;
+onscroll = function (ev) {
+    var ev = ev || event;
+    var top = document.documentElement.scrollTop || document.body.scrollTop;
 
-		if(top>twoDiv.offsetTop-65){
-			aUl.style.backgroundColor = 'rgba(0,0,0,0.3)';
-            pullDown.style.backgroundColor='rgba(0,0,0,0.3)';
-            pullDown.style.Top='64px';
-            nav_ul.style.backgroundColor='rgba(0,0,0,0.3)';
-            clicDown.style.backgroundColor='rgba(0,0,0,0.3)';
-		  }else{
-			aUl.style.backgroundColor='rgba(0,0,0,0)';
-            pullDown.style.backgroundColor='rgba(0,0,0,0)';
-            pullDown.style.Top='64px';
-            nav_ul.style.backgroundColor='rgba(0,0,0,0)';
-            clicDown.style.backgroundColor='rgba(0,0,0,0)';
-		  }
-			
-		};
+    if (top > twoDiv.offsetTop - 65) {
+        aUl.style.backgroundColor = 'rgba(0,0,0,0.3)';
+        pullDown.style.backgroundColor = 'rgba(0,0,0,0.3)';
+        pullDown.style.Top = '64px';
+        nav_ul.style.backgroundColor = 'rgba(0,0,0,0.3)';
+        clicDown.style.backgroundColor = 'rgba(0,0,0,0.3)';
+    } else {
+        aUl.style.backgroundColor = 'rgba(0,0,0,0)';
+        pullDown.style.backgroundColor = 'rgba(0,0,0,0)';
+        pullDown.style.Top = '64px';
+        nav_ul.style.backgroundColor = 'rgba(0,0,0,0)';
+        clicDown.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+
+};
 
 
 //移动端导航事件-------------------------------------
-   var nav_ul = document.getElementById('xnav_ul');
-   var navLi = nav_ul.getElementsByTagName('li');
-   var phone_open = document.getElementById('ph_open');
-   var heig = navLi[0].offsetHeight;
+var nav_ul = document.getElementById('xnav_ul');
+var navLi = nav_ul.getElementsByTagName('li');
+var phone_open = document.getElementById('ph_open');
+var heig = navLi[0].offsetHeight;
 
-   var ntrn = true;
-   nav_ul.style.transition = '0.5s all ease';
-  
-   var phOpen = function phshow(e){
+var ntrn = true;
+nav_ul.style.transition = '0.5s all ease';
 
-      if(ntrn == true){
+var phOpen = function phshow(e) {
+
+    if (ntrn == true) {
         e.style.height = heig + 'px';
         e.style.opacity = 1;
         e.style.overFlow = '';
         ntrn = false
-      }else{
+    } else {
         e.style.height = 0;
         e.style.opacity = 0;
         e.style.overFlow = 'hidden';
         ntrn = true
-      }
+    }
 
-   };
+};
 
-  phone_open.onclick = function(){
-  	   phOpen(nav_ul)
-  }
+phone_open.onclick = function () {
+    phOpen(nav_ul)
+}
 
 var clicDown = document.getElementById("clic_down");
 var clicP = clicDown.getElementsByTagName('p');
 var clHeight = clicP[0].offsetHeight;
-var clicA= navLi[2].getElementsByTagName('a')[0];
+var clicA = navLi[2].getElementsByTagName('a')[0];
 var tim;
-navLi[2].onmouseover = function(){
+navLi[2].onmouseover = function () {
     clearTimeout(tim);
     clicDown.style.height = clHeight + 'px';
     clicA.style.borderBottom = '1px solid #fff';
 };
 
-clicDown.onmouseover = function(){
+clicDown.onmouseover = function () {
     clearTimeout(tim);
-    clicDown.style.height = clHeight+ 'px';
+    clicDown.style.height = clHeight + 'px';
     clicA.style.borderBottom = '1px solid #fff';
 };
-clicDown.onmouseout = function(){
+clicDown.onmouseout = function () {
     clearTimeout(tim);
 
     clicDown.style.height = 0;
-    clicDown.style.overFlow= 'hidden';
+    clicDown.style.overFlow = 'hidden';
     clicA.style.borderBottom = '0px solid #fff';
 };
-navLi[2].onmouseout = function(){
-    tim = setTimeout(function(){
+navLi[2].onmouseout = function () {
+    tim = setTimeout(function () {
 
         clicDown.style.height = 0;
-        clicDown.style.overFlow= 'hidden';
+        clicDown.style.overFlow = 'hidden';
         clicA.style.borderBottom = '0px solid #fff';
-    },700)
+    }, 700)
 };
 
 //更换图片-----------------------------------------
-     var chanSrc = document.getElementById('change_src');
-  
-    var chanImg = function change(e,a){
-    	
-    	e.src=a;
-    	
-    };
- 
-    chanSrc.onmouseover = function(){
-    	chanImg(chanSrc,'images/lo.png');
-    };
-    chanSrc.onmouseout = function(){
-    	chanImg(chanSrc,'images/logo.png');
-    };
-    
- 
+var chanSrc = document.getElementById('change_src');
 
+var chanImg = function change(e, a) {
+
+    e.src = a;
+
+};
+
+chanSrc.onmouseover = function () {
+    chanImg(chanSrc, 'images/lo.png');
+};
+chanSrc.onmouseout = function () {
+    chanImg(chanSrc, 'images/logo.png');
+};
 
 
 // 轮播图
-    var tabUl = document.getElementById('tab_ul');
-	var allInput = tabUl.getElementsByTagName('li');
-    var allSpan = tabUl.getElementsByTagName('span');
-	var tabFirst = document.getElementById('first');
-	var allDiv  = tabFirst.getElementsByTagName('div');
+var tabUl = document.getElementById('tab_ul');
+var allInput = tabUl.getElementsByTagName('li');
+var allSpan = tabUl.getElementsByTagName('span');
+var tabFirst = document.getElementById('first');
+var allDiv = tabFirst.getElementsByTagName('div');
 
-	var allIndex = 0;
-	var timer = null;
-	time();
+var allIndex = 0;
+var timer = null;
+time();
 
-	first.onmouseover = function(){
-		clearInterval(timer);
-	};
-	first.onmouseout = function(){
-		time();
-	}
+first.onmouseover = function () {
+    clearInterval(timer);
+};
+first.onmouseout = function () {
+    time();
+}
 
 
-	for(var i = 0;i < allInput.length;i++){
-     
-		allInput[i].index = i; //记录值
+for (var i = 0; i < allInput.length; i++) {
 
-		allInput[i].onclick = function(){
-			for(var i = 0; i < allInput.length;i++){
-				allSpan[i].className = '';
-				allDiv[i].style.display = 'none';
-			}
-			allDiv[this.index].style.display = 'block';
-            allSpan[this.index].className = 'active';
-			allIndex = this.index;
-		}
-	};
+    allInput[i].index = i; //记录值
 
-	function startMove(){
-		for(var i = 0; i < allInput.length;i++){
+    allInput[i].onclick = function () {
+        for (var i = 0; i < allInput.length; i++) {
             allSpan[i].className = '';
-				allDiv[i].style.display = 'none';
-			}
+            allDiv[i].style.display = 'none';
+        }
+        allDiv[this.index].style.display = 'block';
+        allSpan[this.index].className = 'active';
+        allIndex = this.index;
+    }
+}
+;
 
-		allDiv[allIndex].style.display = 'block';
-        allSpan[allIndex].className = 'active';
-	}
+function startMove() {
+    for (var i = 0; i < allInput.length; i++) {
+        allSpan[i].className = '';
+        allDiv[i].style.display = 'none';
+    }
 
-	function time(){
-		timer = setInterval(function(){
-			allIndex++;
-			if(allIndex == allInput.length)allIndex=0;
-			startMove();
-		},1500);
-	}
+    allDiv[allIndex].style.display = 'block';
+    allSpan[allIndex].className = 'active';
+}
+
+function time() {
+    timer = setInterval(function () {
+        allIndex++;
+        if (allIndex == allInput.length)allIndex = 0;
+        startMove();
+    }, 1500);
+}
 
 //延时导航---------------------------------------------
 var noUL = document.getElementById("no_ul");
@@ -219,134 +223,134 @@ var pullLi = pullDown.getElementsByTagName('li');
 var pullAcol = pullDown.getElementsByTagName('a');
 var pullHeight = pullLi[0].offsetHeight;
 var tim;
-    downLi.onmouseover = function(){
-        clearTimeout(tim);
-        downA.style.borderBottom = '1px solid #fff'
-        pullDown.style.height = pullHeight + "px";
-    };
+downLi.onmouseover = function () {
+    clearTimeout(tim);
+    downA.style.borderBottom = '1px solid #fff'
+    pullDown.style.height = pullHeight + "px";
+};
 
-pullDown.onmouseover = function(){
-     clearTimeout(tim);
+pullDown.onmouseover = function () {
+    clearTimeout(tim);
     pullDown.style.display = "block";
     pullDown.style.height = pullHeight + "px";
     downA.style.borderBottom = '1px solid #fff'
-  };
-pullDown.onmouseout = function(){
+};
+pullDown.onmouseout = function () {
     clearTimeout(tim);
 
     pullDown.style.height = 0;
     pullDown.style.overFlow = 'hidden';
     downA.style.borderBottom = '0px solid #fff'
- };
-downLi.onmouseout = function(){
-    tim = setTimeout(function(){
+};
+downLi.onmouseout = function () {
+    tim = setTimeout(function () {
 
         pullDown.style.height = 0;
         pullDown.style.overFlow = 'hidden';
         downA.style.borderBottom = '0px solid #fff'
-    },700)
+    }, 700)
 };
 //第四屏动画-------------------------------------------
 
-  var ulWidth  = $('#forth_ul li').width();
-  function por(){
-    if($(window).width()>1349){
-      ulWidth = 440;
-      $("#forth_ul").animate({left:-ulWidth},1000,function(){
-           console.log($("#forth_ul")[0].offsetLeft);
-        $(this).css("left","0").find(".oone:first").appendTo("#forth_ul") ;
-      })
-  }
-  else if($(window).width()==1349){
-     ulWidth = 380;
+var ulWidth = $('#forth_ul li').width();
+function por() {
+    if ($(window).width() > 1349) {
+        ulWidth = 440;
+        $("#forth_ul").animate({left: -ulWidth}, 1000, function () {
+            console.log($("#forth_ul")[0].offsetLeft);
+            $(this).css("left", "0").find(".oone:first").appendTo("#forth_ul");
+        })
+    }
+    else if ($(window).width() == 1349) {
+        ulWidth = 380;
 
-      $("#forth_ul").animate({left:-ulWidth},1000,function(){
+        $("#forth_ul").animate({left: -ulWidth}, 1000, function () {
 
-        $(this).css("left","0").find(".oone:first").appendTo("#forth_ul") ;           
-      })
-  }
-  else if($(window).width()<1349){
-      ulWidth = $('#forth_ul li').width()+parseInt( $('#forth_ul li').css('marginLeft') );
-      $("#forth_ul").animate({left:-ulWidth},1000,function(){
+            $(this).css("left", "0").find(".oone:first").appendTo("#forth_ul");
+        })
+    }
+    else if ($(window).width() < 1349) {
+        ulWidth = $('#forth_ul li').width() + parseInt($('#forth_ul li').css('marginLeft'));
+        $("#forth_ul").animate({left: -ulWidth}, 1000, function () {
 
-        $(this).css("left","0").find(".oone:first").appendTo("#forth_ul") ;           
-      }) 
-     }
-               
-    };
-  
-  //点击事件    
-     $(".onleft").click(function(){
-        por()
-     })
+            $(this).css("left", "0").find(".oone:first").appendTo("#forth_ul");
+        })
+    }
 
-     $(".onright").click(function(){
-         // $('.forth_ul').stop().animate({left:'+='+ulWidth},1000,function(){
-         //  //console.log( $("#forth_ul")[0].offsetLeft)
-         // $('.forth_ul').css("left","-="+ulWidth).stop().find(".oone:last-child").prependTo(".forth_ul");
-          $('.forth_ul').css('left','-='+ulWidth).stop().find(".oone:last-child").prependTo(".forth_ul").parent().stop().animate({left:'+='+ulWidth},1000,function(){})
+};
 
-     });
+//点击事件
+$(".onleft").click(function () {
+    por()
+})
+
+$(".onright").click(function () {
+    // $('.forth_ul').stop().animate({left:'+='+ulWidth},1000,function(){
+    //  //console.log( $("#forth_ul")[0].offsetLeft)
+    // $('.forth_ul').css("left","-="+ulWidth).stop().find(".oone:last-child").prependTo(".forth_ul");
+    $('.forth_ul').css('left', '-=' + ulWidth).stop().find(".oone:last-child").prependTo(".forth_ul").parent().stop().animate({left: '+=' + ulWidth}, 1000, function () {
+    })
+
+});
 
 //rember me
 
 var rem = document.getElementById('rember');
 var clic = true;
-rem.onclick=function(){
-     if(clic==true){
-      dot.style.float='right';        
-          clic=false;
-     }else{
-        dot.style.float='left';
-        clic=true;
-     }
+rem.onclick = function () {
+    if (clic == true) {
+        dot.style.float = 'right';
+        clic = false;
+    } else {
+        dot.style.float = 'left';
+        clic = true;
+    }
 }
-
 
 
 // 是否在当前屏jq---------------------------------
 
 $(document).ready(function () {
-            
-            $(window).scroll(function () {
-                var a = document.getElementById("cen_right").offsetTop;
-             
-                if (a >= $(window).scrollTop() && a < ($(window).scrollTop()+$(window).height())) {
-                
-                    $('.cen_right').addClass('opta')
-                }else{
-             
-                    $('.cen_right').removeClass('opta')
-                }
 
-            });
+    $(window).scroll(function () {
+        var a = document.getElementById("cen_right").offsetTop;
 
-             $(window).scroll(function () {
-                var a = document.getElementById("cen_left").offsetTop;
-                if (a >= $(window).scrollTop() && a < ($(window).scrollTop()+$(window).height())-100) {                  
-                    $('.th_right').addClass('ancen')
-                     $('#cen_left').addClass('ancen')
-                }else{
-                	  
-                      $('.th_right').removeClass('ancen')
-                      $('#cen_left').removeClass('ancen')
-                      
-                }
+        if (a >= $(window).scrollTop() && a < ($(window).scrollTop() + $(window).height())) {
 
-            });
+            $('.cen_right').addClass('opta')
+        } else {
 
-             $(window).scroll(function () {
-                var a = document.getElementById("footer_center").offsetTop;
-                if (a >= $(window).scrollTop() && a < ($(window).scrollTop()+$(window).height())) {
-                   
-                    $('.footer_center').addClass('bottom')
-                }else {
-                	  
-                      $('.footer_center').removeClass('bottom')
-                      
-                }
+            $('.cen_right').removeClass('opta')
+        }
 
-            });
+    });
+
+    $(window).scroll(function () {
+        var a = document.getElementById("cen_left").offsetTop;
+        if (a >= $(window).scrollTop() && a < ($(window).scrollTop() + $(window).height()) - 100) {
+            $('.th_right').addClass('ancen')
+            $('#cen_left').addClass('ancen')
+        } else {
+
+            $('.th_right').removeClass('ancen')
+            $('#cen_left').removeClass('ancen')
+
+        }
+
+    });
+
+    $(window).scroll(function () {
+        var a = document.getElementById("footer_center").offsetTop;
+        if (a >= $(window).scrollTop() && a < ($(window).scrollTop() + $(window).height())) {
+
+            $('.footer_center').addClass('bottom')
+        } else {
+
+            $('.footer_center').removeClass('bottom')
+
+        }
+
+    });
 
 });
 
