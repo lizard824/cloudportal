e1ec96dbca4047dc3d4dc7dd41ffdf9aa75c785c
duanxc1 <duanxc1@lenovo.com>
2017-01-12 16:46:25 +0800

    change url

--- js/login.js
+++ js/login.js
@@ -63,7 +63,7 @@ rem.onclick = function () {
         clic = true;
     }
 };
-var _CTX_ = 'http://test.lenovo.com:8180/sso';
+var _CTX_ = 'http://sso.earth.xpaas.lenovo.com';
 var v_login;
 var refer = getParameterByName("refer", window.location);
 $(document).ready(function () {
