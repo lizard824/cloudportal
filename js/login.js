027f935a4b454e4379f15039f1833b2a494c3d07
liuxiaohui <17310036029@163.com>
2017-01-25 11:15:22 +0800

    html css

--- js/login.js
+++ js/login.js
@@ -4,45 +4,6 @@ onresize = function () {
     document.documentElement.style.fontSize = innerWidth / 16 + 'px';
 };
 
-// 登录注册切换中英文---------------------------------
-// 登录 窗口打开
-// var login  = document.getElementById('login');
-// var log    = document.getElementById('log');
-// var wlog   = document.getElementById('wlog');
-// //var sign   = document.getElementById('sign');
-// var signbj = document.getElementById('signbj');
-// var sig    = document.getElementById('sig');
-
-// var clLig = function bl(a,b,c,d){
-//          a.style.display = "block";
-//          b.style.display = "block";
-//          c.style.display = "none";
-//          d.style.display = "none";
-//     };
-// login.onclick = function(){
-//       clLig(log,wlog,signbj,sig);
-//     };
-//sign.onclick = function(){
-//      clLig(signbj,sig,log,wlog);
-//};
-
-
-// 关闭-------------------------------------------------
-// var clos = document.getElementById('close');
-// var closig = document.getElementById('closesig');
-
-// // 关闭窗口   
-// var myClo = function cl(a, b) {
-//     a.style.display = "none";
-//     b.style.display = "none";
-// };
-
-// clos.onclick = function () {
-//     myClo(log, wlog);
-// };
-// closig.onclick = function () {
-//     myClo(signbj, sig);
-// };
 
 //rember me
 
