cb56983b259bf04d209e93ad561cf8b77e36db3e
liuxiaohui <17310036029@163.com>
2017-04-19 18:07:46 +0800

    change password outside

--- js/my.js
+++ js/my.js
@@ -21,59 +21,7 @@ onresize = function () {
 // anClos.onclick = function(){
 //      anClose(anMite);
 // };
-//修改密码关闭
-function chanBlock(e){
-    e.style.display = 'block';
-}
-function chanClose(e){
-    e.style.display = 'none';
-}
-var moDify = document.getElementById('modify');
-var canCel = document.getElementById('cancel');
-var changeWord = document.getElementById('change-word');
-changeWord.onclick = function(){
-    chanBlock(moDify);
-};
-canCel.onclick = function(){
-    chanClose(moDify);
-};
-//修改密码input动画
-var moP = document.getElementById('mo-p');
-var movP = document.getElementById('mov-p');
-var moap = document.getElementById('moa-p')
-var newWord = document.getElementById('new-word');
-var conWord = document.getElementById('con-word');
-var oldWord = document.getElementById('old-word');
-var moSub = document.getElementById('mo-sub');
 
-function moveP(e,a,c){
-    e.style.top = a;
-    e.style.fontSize = c;
-}
-newWord.onfocus = function(){
-    moveP(moP,'-5px','14px');
-    };   
-newWord.onblur = function(){
-    if(newWord.value == ''){
-       moveP(moP,'20px','16px');            
-        };      
-    };
-conWord.onfocus = function(){
-    moveP(movP,'-5px','14px');
-    };   
-conWord.onblur = function(){
-    if(conWord.value == ''){
-       moveP(movP,'20px','16px');            
-        };
-    };
-oldWord.onfocus = function(){
-    moveP(moap,'-5px','14px');
-};
-oldWord.onblur = function(){
-    if(conWord.value == ''){
-        moveP(moap,'20px','16px');
-    };
-};
 //鼠标滚动
 var agent = navigator.userAgent;
 if (/.*Firefox.*/.test(agent)) {
